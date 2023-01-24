8b2e7bcf345f0b6c4ace670dd3f13fdc9daacb9a
Luiz Felipe <luizfelipelimafurtado@gmail.com>
2023-01-24 18:23:53 -0300

    cadastro de user

--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path('login/', views.login, name='login'),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('logout/', views.logout, name='sair'),  # sempre retorna alguma coisa
+
 ]
--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -6,6 +6,19 @@ from django.contrib.auth.decorators import login_required
 from perguntas.forms import FormPergunta, FormEscolha
 
 # Create your views here.
+def cadastro(request):
+    if request.method == 'GET':
+        return render(request, 'forms/cadastro_form.html')
+
+    nome = request.POST.get('nome')
+    sobrenome = request.POST.get('sobrenome')
+    usuario = request.POST.get('usuario')
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+    senha_2 = request.POST.get('senha2')
+    # print(nome, sobrenome, usuario, email, senha, senha_2)
+    return render(request, 'forms/cadastro_form.html')
+
 def login(request):
     if request.method != 'POST':
         return render(request, 'accounts_pages/login.html')
@@ -13,22 +26,20 @@ def login(request):
     usuario = request.POST.get('user')
     senha = request.POST.get('senha')
     user = auth.authenticate(request, username=usuario, password=senha)
-    if not user:
-        print('acessado!!')
+    if user:
         return redirect('perguntas:index')
     else:
-        print('Negado')
         return redirect('accounts:login')
-    return redirect('perguntas:index')
+
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect('login')
 
-def cadastro(request):
-    pass
 
 def adicionar_pergunta(request):
     pass
+
 
 def dashboard(request):
     pass
